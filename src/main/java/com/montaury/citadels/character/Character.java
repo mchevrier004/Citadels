49056c44b42317661a35537a6e5930033ce16a72
sid marin <sidm1109@gmail.com>
2020-02-14 10:13:13 +0100

    ajout du pouvoir des personnages dans la classe personnage

--- src/main/java/com/montaury/citadels/character/Character.java
+++ src/main/java/com/montaury/citadels/character/Character.java
@@ -1,27 +1,29 @@
 package com.montaury.citadels.character;
 
 import com.montaury.citadels.district.DistrictType;
+import io.vavr.collection.List;
 import io.vavr.control.Option;
 
 public enum Character {
-    ASSASSIN(1, "Assassin"),
-    THIEF(2, "Thief"),
-    MAGICIAN(3, "Magician"),
-    KING(4, "King", DistrictType.NOBLE),
-    BISHOP(5, "Bishop", DistrictType.RELIGIOUS),
-    MERCHANT(6, "Merchant", DistrictType.TRADE),
-    ARCHITECT(7, "Architect"),
-    WARLORD(8, "Warlord", DistrictType.MILITARY);
+    ASSASSIN(1, "Assassin", List.of("Kill")),
+    THIEF(2, "Thief", List.of("Rob")),
+    MAGICIAN(3, "Magician", List.of("Exchange cards with other player", "Exchange cards with pile")),
+    KING(4, "King", List.of("Receive income"), DistrictType.NOBLE),
+    BISHOP(5, "Bishop", List.of("Receive income"), DistrictType.RELIGIOUS),
+    MERCHANT(6, "Merchant", List.of("Receive income", "Receive 1 gold"), DistrictType.TRADE),
+    ARCHITECT(7, "Architect", List.of("Pick 2 cards", "Build district", "Build district")),
+    WARLORD(8, "Warlord", List.of("Receive income", "Destroy district"), DistrictType.MILITARY);
 
-    Character(int number, String name)
+    Character(int number, String name, List<String> powers)
     {
-        this(number, name, null);
+        this(number, name, powers, null);
     }
 
-    Character(int number, String name, DistrictType associatedDistrictType)
+    Character(int number, String name,List<String> powers, DistrictType associatedDistrictType)
     {
         this.number = number;
         this.name = name;
+        this.powers = powers;
         this.associatedDistrictType = Option.of(associatedDistrictType);
     }
 
@@ -33,11 +35,14 @@ public enum Character {
         return name;
     }
 
+    public List<String> getPowers() { return powers;}
+
     public Option<DistrictType> associatedDistrictType() {
         return associatedDistrictType;
     }
 
     private final int number;
     private final String name;
+    private final List<String> powers;
     private final Option<DistrictType> associatedDistrictType;
 }
